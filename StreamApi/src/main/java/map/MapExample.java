70be7c813808d4fd291500d5b32b4417eaa3ff00
Aditya Raj Verma <adityarajverma@gmail.com>
2020-09-23 20:43:17 +0530

    02. Map and Collect

---
+++ StreamApi/src/main/java/map/MapExample.java
@@ -0,0 +1,63 @@
+package map;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MapExample {
+
+	public static void main(String args[]) {
+
+		List<String> names = Arrays.asList("Aditya", "Rahul", "Ajeet", "sharukh", "Verma");
+		names.stream().filter(name -> !name.equals("Aditya")).map(name -> new User(name))
+				.forEach(user -> System.out.println(user));
+
+		// map have consume Function<T,R>
+		// Function have only one unimplemented method
+		// R apply(T t);
+
+		// Now if we want to collect the Stream instead of printing or doing any
+		// terminal operation which end in printing it.
+		// It simply means we want to persisit the data in any collection for further
+		// uses.
+
+		List<User> userList = names.stream().filter(name -> !name.equals("Aditya")).map(name -> new User(name))
+				.collect(Collectors.toList());
+
+		System.out.println("Printed after collectors");
+		System.out.println(userList);
+
+	}
+
+}
+
+class User {
+	private String name;
+	private String age = "30";
+
+	public User(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + "]";
+	}
+}
